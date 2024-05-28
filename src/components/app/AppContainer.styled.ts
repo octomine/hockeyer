b35699e1174d8cb4a743da7df9c61c1356fef7b3
octo <v.minaev@qiwi.com>
2024-05-28 16:44:49 +0300

    prettier

--- src/components/app/AppContainer.styled.ts
+++ src/components/app/AppContainer.styled.ts
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-position: relative;
+  position: relative;
   display: flex;
   flex-direction: column;
   height: 100vh;
 `;
 
 export const UIHolder = styled.div`
-pointer-events: none;
-position: absolute;
-width: 100%;
-height: 100%;
-`
+  pointer-events: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
