246426024b262c44c78861fbacad3960f79d4532
octo <v.minaev@qiwi.com>
2024-06-04 13:50:34 +0300

    some refactor

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -2,13 +2,11 @@ import { initGame } from '@app/game';
 import { useEffect, useState } from 'react';
 
 const Game = () => {
-  const [game, setGame] = useState<Phaser.Game>();
+  const [, setGame] = useState<Phaser.Game>();
 
   useEffect(() => {
     setGame(initGame());
   }, []);
-
-  console.log(game);
 
   return <div id="game"></div>;
 };
