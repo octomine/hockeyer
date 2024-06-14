93c5b941cbba30f023e23e303f48ae2043a5182d
octo <v.minaev@qiwi.com>
2024-06-14 11:58:22 +0300

    some better ui

---
+++ src/components/messages/Messages.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding: 1em 2em;
+  border: 2px solid #cccccc;
+  border-radius: 4px;
+  backdrop-filter: blur(5px);
+  box-shadow: 0 1px 4px #00000088;
+`;
--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -2,19 +2,24 @@ import { IState, UIMessage } from '@app/slices/types';
 import { useSelector } from 'react-redux';
 import { LevelFinish } from './level-finish';
 import { LevelUp } from './level-up';
+import { Wrapper } from './Messages.styled';
 
 const Messages = () => {
   const message = useSelector<IState, UIMessage>((state) => state.UI.message);
 
-  switch (message) {
-    case UIMessage.LevelFinish:
-      return <LevelFinish></LevelFinish>;
-    case UIMessage.LevelUp:
-      return <LevelUp></LevelUp>;
-    case UIMessage.None:
-    default:
-      return null;
-  }
+  const msg = () => {
+    switch (message) {
+      case UIMessage.LevelFinish:
+        return <LevelFinish></LevelFinish>;
+      case UIMessage.LevelUp:
+        return <LevelUp></LevelUp>;
+      case UIMessage.None:
+      default:
+        return null;
+    }
+  };
+
+  return <Wrapper>{msg()}</Wrapper>;
 };
 
 export default Messages;
