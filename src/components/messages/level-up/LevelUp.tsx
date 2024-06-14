d2b6d132d903a0949eeb59edc3738bf3a74ea405
octo <v.minaev@qiwi.com>
2024-06-14 13:35:59 +0300

    some better

--- src/components/messages/level-up/LevelUp.tsx
+++ src/components/messages/level-up/LevelUp.tsx
@@ -1,5 +1,5 @@
-import { resetTime, setIsPlaying } from '@app/slices';
-import { IState } from '@app/slices/types';
+import { resetTime, setIsPlaying, showMessage } from '@app/slices';
+import { IState, UIMessage } from '@app/slices/types';
 import { useDispatch, useSelector } from 'react-redux';
 
 const LevelUp = () => {
@@ -7,6 +7,7 @@ const LevelUp = () => {
   const level = useSelector<IState, number>((state) => state.level.level);
   // TODO: запилить нормальный countdow или придумать что получше
   setTimeout(() => {
+    dispatch(showMessage(UIMessage.None))
     dispatch(resetTime());
     dispatch(setIsPlaying(true));
   }, 3 * 1000);
